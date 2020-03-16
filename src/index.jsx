b38f1fa7460a033a4193e620e5804178e1bc511b
Yaroslav Losev <losevyaroslav@gmail.com>
2020-03-16 22:25:41 +0300

    Implement renderer

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import { render } from './renderer';
 
 const App = () => {
+    const [color, setColor] = React.useState(0);
+    const colors = ['#ff0000', '#f0f0f0', '#abcdef'];
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setColor(prev => prev + 1);
+        }, 500);
+    });
+
     return (
         <frame name="Awesome frame" height={200} width={100}>
             <rectangle
                 name="R1"
                 width={100}
                 height={100}
-                backgroundColor="#ff00ff"
+                backgroundColor={colors[color % 3]}
             />
+
             <rectangle
                 name="R2"
                 width={100}
--- src/renderer.js
+++ src/renderer.js
@@ -1,3 +1,83 @@
+import Reconciler from 'react-reconciler';
+import { backgroundColorAssign } from './helpers';
+
+const hostConfig = {
+    now: Date.now,
+    supportsMutation: true,
+
+    createInstance: (type, props) => {
+        let instance;
+
+        if (type === 'rectangle') {
+            instance = figma.createRectangle();
+        } else if (type === 'frame') {
+            instance = figma.createFrame();
+        }
+
+        ['name', 'x', 'y'].forEach(value => {
+            if (props[value]) {
+                instance[value] = props[value];
+            }
+        });
+
+        instance.resize(
+            props['width'] || instance.width,
+            props['height'] || instance.height
+        );
+
+        if (props['backgroundColor']) {
+            backgroundColorAssign(instance, props['backgroundColor']);
+        }
+
+        return instance;
+    },
+
+    appendInitialChild: (parentInstance, child) => {
+        parentInstance.appendChild(child);
+    },
+    appendChildToContainer: (container, child) => {
+        container.appendChild(child);
+    },
+    appendChild: (parentInstance, child) => {
+        parentInstance.appendChild(child);
+    },
+
+    removeChild: (parentInstance, child) => {
+        child.remove();
+    },
+    removeChildFromContainer: (container, child) => {
+        child.remove();
+    },
+
+    insertBefore: (parentInstance, child, beforeChild) => {
+        const index = parentInstance.children.indexOf(beforeChild);
+        parentInstance.insertChild(index, child);
+    },
+    insertInContainerBefore: (container, child, beforeChild) => {
+        const index = container.children.indexOf(beforeChild);
+        container.insertChild(index, child);
+    },
+
+    prepareUpdate: (node, type, oldProps, newProps) => {
+        return newProps;
+    },
+    commitUpdate: (instance, updatePayload, type, oldProps, newProps) => {
+        if (updatePayload['backgroundColor']) {
+            backgroundColorAssign(instance, updatePayload['backgroundColor']);
+        }
+    },
+
+    getRootHostContext: rootContainerInstance => null,
+    getChildHostContext: (parentHostContext, type, rootContainerInstance) =>
+        null,
+    prepareForCommit: rootContainerInstance => {},
+    resetAfterCommit: rootContainerInstance => {},
+    shouldSetTextContent: () => false,
+    finalizeInitialChildren: () => false
+};
+
 export const render = (jsx, rootNode) => {
-    console.log('Hello from React to Figma renderer');
+    const reconciler = Reconciler(hostConfig);
+    const container = reconciler.createContainer(rootNode);
+    reconciler.updateContainer(jsx, container);
 };
